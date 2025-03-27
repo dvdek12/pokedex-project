41dc481ea6ac60285336b4e5a3d6e3ea6b1026f3
Przemyslaw Dudek <przemekdudek321@gmail.com>
2025-03-27 20:00:49 +0100

    usunieto niepotrzebny plik

--- scripts/pokedex.js
+++ scripts/pokedex.js
@@ -1,7 +1,3 @@
-import { getPokemonFromAPI } from "../utils/get-pokemon-from-api"
-
-
-
 const commonPokemonNames = ['Pikachu ', 'Charizard', 'Bulbasaur', 'Squirtle', 'Jigglypuff', 'Gengar', 'Eevee', 'Snorlax', 'Lucario', 'Mewtwo']
 let commonPokemons = []
 
@@ -43,6 +39,23 @@ document.addEventListener('DOMContentLoaded', async () => {
 let carouselItems = ['1', '2', '3']
 const generateCarousel = () => {
 
+}
+
+const getPokemonFromAPI = async (pokemon) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+
+      return json
+
+    } catch (error) {
+      console.error(error.message);
+    }
 }
 
 
